a17ab1d7788f2af227ed2f0febbd6258572dccc2
asafridehossain171@gmail.com <asafridehossain171@gmail.com>
2023-05-03 16:23:35 +0600

    Error page added to the project

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,14 @@
+const ErrorPage = () => {
+  const errorImage = `https://raw.githubusercontent.com/AfrideHossain/images/main/errorIllustration/11104-removebg-preview.png`;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-200">
+      <img src={errorImage} alt="Error" className="mb-8 w-64" />
+      <h1 className="text-4xl font-bold mb-4">Oops! Something went wrong.</h1>
+      <p className="text-lg text-gray-700">
+        We apologize for the inconvenience. Please try again later.
+      </p>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../components/Layout/Main";
 import Home from "../components/Home/Home";
 import ChefRecipes from "../components/ChefRecipes/ChefRecipes";
+import ErrorPage from "../components/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
