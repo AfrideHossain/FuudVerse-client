92d1673205dafcc07af7297df0859c52ce429760
asafridehossain171@gmail.com <asafridehossain171@gmail.com>
2023-05-03 21:37:02 +0600

    React toastify installed and added to the project

--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -1,10 +1,24 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Main = () => {
   return (
     <>
       <Navbar />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <Outlet />
     </>
   );
--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,4 +1,5 @@
 import { HeartIcon } from "@heroicons/react/24/outline";
+import { toast } from "react-toastify";
 const RecipeCard = () => {
   const sampleRecipe = {
     recipe_name: "Classic Spaghetti Bolognese",
@@ -21,35 +22,51 @@ const RecipeCard = () => {
     rating: 4.5,
     favorite: false,
   };
-
+  const handleHeart = () => {
+    toast.success("Yey, Toast showing", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  };
   return (
-    <div className="flex flex-col md:flex-row bg-gray-100 max-w-full md:w-[992px] m-4 p-4 rounded-lg shadow-md border border-violet-100">
-      <div className="ps-3 mt-4">
-        <h1 className="text-2xl text-gray-800 font-bold mb-2">
-          Classic Spaghetti Bolognese
-        </h1>
-        <p className="text-base font-medium text-gray-600">
-          {sampleRecipe.instructions}
-        </p>
-        <div className="text-lg font-medium text-gray-600 grid grid-cols-1 md:grid-cols-3 mt-4">
-          {sampleRecipe.ingredients.map((ingredient, indx) => {
-            return (
-              <li className="text-sm" key={indx}>
-                {ingredient}
-              </li>
-            );
-          })}
-        </div>
-        <div className="w-full mt-5 flex justify-end items-start gap-2">
+    <>
+      <div className="flex flex-col md:flex-row bg-gray-100 max-w-full md:w-[992px] m-4 p-4 rounded-lg shadow-md border border-violet-100">
+        <div className="ps-3 mt-4">
+          <h1 className="text-2xl text-gray-800 font-bold mb-2">
+            Classic Spaghetti Bolognese
+          </h1>
           <p className="text-base font-medium text-gray-600">
-            Make it favorite
+            {sampleRecipe.instructions}
           </p>
-          <button className="text-violet-600 hover:text-violet-700 flex items-center justify-center">
-            <HeartIcon className="w-6 h-6" />
-          </button>
+          <div className="text-lg font-medium text-gray-600 grid grid-cols-1 md:grid-cols-3 mt-4">
+            {sampleRecipe.ingredients.map((ingredient, indx) => {
+              return (
+                <li className="text-sm" key={indx}>
+                  {ingredient}
+                </li>
+              );
+            })}
+          </div>
+          <div className="w-full mt-5 flex justify-end items-start gap-2">
+            <p className="text-base font-medium text-gray-600">
+              Make it favorite
+            </p>
+            <button
+              onClick={handleHeart}
+              className="text-violet-600 hover:text-violet-700 flex items-center justify-center"
+            >
+              <HeartIcon className="w-6 h-6" />
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
